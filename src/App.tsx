0c5af5174a0720171815dee02d05e0ce53fa6f4d
Andrew Petersen <andrew.petersen15@gmail.com>
2020-03-28 17:12:34 -0500

    Got snowpack setup with typescript and react and sass

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "/web_modules/react.js";
+
+function App({}) {
+  return (
+    <div className="app">
+      <h1>App Updated!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/entry.tsx
@@ -0,0 +1,6 @@
+import React from "/web_modules/react.js";
+import ReactDOM from "/web_modules/react-dom.js";
+
+import App from "./App.js";
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
