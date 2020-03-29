16ed8cf702fb680853ec04d1191a2752927d5a91
Andrew Petersen <andrew.petersen15@gmail.com>
2020-03-29 00:53:51 -0500

    got graphql calls working with a get

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from "/web_modules/react.js";
+import "./data/graphql.js";
+import { gql, useQuery } from "./data/graphql.js";
 
 function App({}) {
+  let result = useQuery(NAVIGATION_QUERY);
+  console.log("Navigation", result);
   return (
     <div className="app">
       <h1>App Updated!</h1>
     </div>
   );
 }
+
+const NAVIGATION_QUERY = gql`
+  query NavigationData {
+    navigation(uid: "navigation", lang: "en-us") {
+      title
+      logo
+      body {
+        ... on NavigationBodyMenu_link {
+          primary {
+            label
+            path
+          }
+        }
+        ... on NavigationBodySocial_media {
+          primary {
+            name
+            link
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default App;
---
+++ src/data/graphql.ts
@@ -0,0 +1,54 @@
+import useAsyncData from "../hooks/useAsyncData.js";
+import { useEffect, useState } from "/web_modules/react.js";
+
+const REPOSITORY = "queenjeannes";
+const GRAPHQL_ENDPOINT = `https://${REPOSITORY}.prismic.io/graphql`;
+const REST_ENDPOINT = `https://${REPOSITORY}.prismic.io/api/v2`;
+
+let masterRef = "";
+
+let waitForRef = (async () => {
+  try {
+    let { refs } = await fetch(REST_ENDPOINT).then(res => res.json());
+    masterRef = refs.find(r => r.id === "master").ref;
+  } catch (err) {
+    console.error("Unable to set master ref");
+  }
+})();
+
+export const gql = str => str;
+
+let request = async function(query, queryOptions = {}) {
+  let url = GRAPHQL_ENDPOINT + "?query=" + encodeURIComponent(query);
+  await waitForRef;
+  let result = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Prismic-ref": masterRef,
+    },
+    // spread the passed in queryOptions onto the POST body
+  }).then(res => res.json());
+  console.log("result", result);
+  return result;
+};
+
+export interface QueryState<T> {
+  data: T;
+  error?: any;
+}
+export const useQuery = function<T>(query) {
+  let [state, setState] = useState<QueryState<T>>({ data: null });
+
+  useEffect(() => {
+    let isMounted = true;
+    let doAsync = async () => {
+      let result = await request(query);
+      if (!isMounted) return;
+      setState(result);
+    };
+    doAsync();
+    return () => (isMounted = false);
+  }, [query]);
+  return state;
+};
