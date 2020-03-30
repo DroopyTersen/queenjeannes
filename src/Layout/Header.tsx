fb33b20fc21bd7514ad4300fa99d6a56df4d4f93
Petersen <apetersen@skylinetechnologies.com>
2020-03-30 16:36:24 -0500

    added caching to useQuery, prettier and favicon

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,78 +1,83 @@
 import React from "/web_modules/react.js";
 import { gql, useQuery } from "../data/graphql.js";
 import {
-	NavigationData,
-	NavigationData_navigation_body_NavigationBodyMenu_link_primary as MenuLink,
-	NavigationData_navigation_body_NavigationBodySocial_media_primary as Social,
+  NavigationData,
+  NavigationData_navigation_body_NavigationBodyMenu_link_primary as MenuLink,
+  NavigationData_navigation_body_NavigationBodySocial_media_primary as Social,
 } from "./_types/NavigationData.js";
 
 function Header({}) {
-	let { logo, menuLinks, socials } = useNavigation();
-	let [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  let { logo, menuLinks, socials } = useNavigation();
+  let [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-	return (
-		<header className="header">
-			<div className="logo">
-				<a href="/">
-					<i className="fas fa-crown" />
-				</a>
-				<h1 className="logo-title">Queen Jeanne's Bakery</h1>
-			</div>
+  return (
+    <header className="header">
+      <div className="logo">
+        <a href="/">
+          <i className="fas fa-crown" />
+        </a>
+        <h1 className="logo-title">Queen Jeanne's Bakery</h1>
+      </div>
 
-			<div className="menu-trigger">
-				<button className="button-clear" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-					{!isMenuOpen && <i className="fas fa-bars"></i>}
-					{isMenuOpen && <i className="fas fa-times"></i>}
-				</button>
-			</div>
-			<nav className={isMenuOpen ? "open" : "closed"}>
-				<h1 className="logo-title">Queen Jeanne's Bakery</h1>
+      <div className="menu-trigger">
+        <button className="button-clear" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {!isMenuOpen && <i className="fas fa-bars"></i>}
+          {isMenuOpen && <i className="fas fa-times"></i>}
+        </button>
+      </div>
+      <nav className={isMenuOpen ? "open" : "closed"}>
+        <h1 className="logo-title">Queen Jeanne's Bakery</h1>
 
-				{menuLinks &&
-					menuLinks.map(link => (
-						<a className="nav-item" href={link.path}>
-							{link.label}
-						</a>
-					))}
-			</nav>
-		</header>
-	);
+        {menuLinks &&
+          menuLinks.map((link) => (
+            <a className="nav-item" href={link.path}>
+              {link.label}
+            </a>
+          ))}
+      </nav>
+    </header>
+  );
 }
 
 export default React.memo(Header);
 
 function useNavigation() {
-	let { data } = useQuery<NavigationData>(NAVIGATION_QUERY);
+  console.log("useNavigation -> NAVIGATION_QUERY", NAVIGATION_QUERY);
+  let { data } = useQuery<NavigationData>(NAVIGATION_QUERY);
 
-	if (!data) return {};
-	return {
-		logo: data.navigation.logo.url + "",
-		menuLinks: data.navigation.body.filter(l => l.type === "menu_link").map(ml => ml.primary) as MenuLink[],
-		socials: data.navigation.body.filter(l => l.type === "social_media").map(ml => ml.primary) as Social[],
-	};
+  if (!data) return {};
+  return {
+    logo: data.navigation.logo.url + "",
+    menuLinks: data.navigation.body
+      .filter((l) => l.type === "menu_link")
+      .map((ml) => ml.primary) as MenuLink[],
+    socials: data.navigation.body
+      .filter((l) => l.type === "social_media")
+      .map((ml) => ml.primary) as Social[],
+  };
 }
 
 const NAVIGATION_QUERY = gql`
-	query NavigationData {
-		navigation(uid: "navigation", lang: "en-us") {
-			title
-			logo
-			body {
-				... on NavigationBodyMenu_link {
-					type
-					primary {
-						label
-						path
-					}
-				}
-				... on NavigationBodySocial_media {
-					type
-					primary {
-						name
-						link
-					}
-				}
-			}
-		}
-	}
+  query NavigationData {
+    navigation(uid: "navigation", lang: "en-us") {
+      title
+      logo
+      body {
+        ... on NavigationBodyMenu_link {
+          type
+          primary {
+            label
+            path
+          }
+        }
+        ... on NavigationBodySocial_media {
+          type
+          primary {
+            name
+            link
+          }
+        }
+      }
+    }
+  }
 `;
--- src/data/graphql.ts
+++ src/data/graphql.ts
@@ -1,5 +1,6 @@
 import useAsyncData from "../hooks/useAsyncData.js";
 import { useEffect, useState } from "/web_modules/react.js";
+import cache, { CacheOptions } from "./cache.js";
 
 const REPOSITORY = "queenjeannes";
 const GRAPHQL_ENDPOINT = `https://${REPOSITORY}.cdn.prismic.io/graphql`;
@@ -8,47 +9,65 @@ const REST_ENDPOINT = `https://${REPOSITORY}.prismic.io/api/v2`;
 let masterRef = "";
 
 let waitForRef = (async () => {
-	try {
-		let { refs } = await fetch(REST_ENDPOINT).then(res => res.json());
-		masterRef = refs.find(r => r.id === "master").ref;
-	} catch (err) {
-		console.error("Unable to set master ref");
-	}
+  try {
+    let { refs } = await fetch(REST_ENDPOINT).then((res) => res.json());
+    masterRef = refs.find((r) => r.id === "master").ref;
+  } catch (err) {
+    console.error("Unable to set master ref");
+  }
 })();
 
-export const gql = str => str;
+export const gql = (str) => {
+  console.log("gql -> str", str);
+  return str[0];
+};
 
 let request = async function(query, queryOptions = {}) {
-	let url = GRAPHQL_ENDPOINT + "?query=" + encodeURIComponent(query);
-	await waitForRef;
-	let result = await fetch(url, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-			"Prismic-ref": masterRef,
-		},
-		// spread the passed in queryOptions onto the POST body
-	}).then(res => res.json());
-	console.log("result", result);
-	return result;
+  console.log("request.query", query);
+  let url = GRAPHQL_ENDPOINT + "?query=" + encodeURIComponent(query);
+  await waitForRef;
+  let result = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Prismic-ref": masterRef,
+    },
+    // spread the passed in queryOptions onto the POST body
+  }).then((res) => res.json());
+  console.log("result", result);
+  return result;
 };
 
 export interface QueryState<T> {
-	data: T;
-	error?: any;
+  data: T;
+  error?: any;
 }
-export const useQuery = function<T>(query) {
-	let [state, setState] = useState({ data: null });
 
-	useEffect(() => {
-		let isMounted = true;
-		let doAsync = async () => {
-			let result = await request(query);
-			if (!isMounted) return;
-			setState(result);
-		};
-		doAsync();
-		return () => (isMounted = false);
-	}, [query]);
-	return state as QueryState<T>;
+const EMPTY_STATE: QueryState<any> = {
+  data: null,
+  error: null,
+};
+export const useQuery = function<T>(query) {
+  let [state, setState] = useState(() => {
+    return query ? cache.get(query) || EMPTY_STATE : EMPTY_STATE;
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+    let doAsync = async () => {
+      let result = cache.get(query);
+      if (!result) {
+        result = await request(query);
+      }
+      if (!isMounted) return;
+      setState(result);
+    };
+    doAsync();
+    return () => (isMounted = false);
+  }, [query]);
+
+  useEffect(() => {
+    cache.set(query, state);
+  }, [state]);
+  return state as QueryState<T>;
 };
