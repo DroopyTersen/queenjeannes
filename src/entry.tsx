f3710bd453352c2dc0819677133c46255b231791
Petersen <apetersen@skylinetechnologies.com>
2020-03-30 09:43:21 -0500

    got theme setup (colors and fonts)

--- src/entry.tsx
+++ src/entry.tsx
@@ -2,5 +2,8 @@ import React from "/web_modules/react.js";
 import ReactDOM from "/web_modules/react-dom.js";
 
 import App from "./App.js";
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+let rootEl = document.getElementById("root");
+ReactDOM.render(React.createElement(App), rootEl);
+setTimeout(() => {
+	rootEl.style.opacity = "1";
+}, 100);
