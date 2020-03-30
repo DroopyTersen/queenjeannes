0eea94267147bd394a3a03f9ffd8ea4b09adc0a0
Petersen <apetersen@skylinetechnologies.com>
2020-03-30 14:44:37 -0500

    got responsive nav kinda working

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,13 @@
 import React from "/web_modules/react.js";
-import "./data/graphql.js";
-import { gql, useQuery } from "./data/graphql.js";
-import { NavigationData } from "./__generated__/NavigationData.js";
+import Header from "./Layout/Header.js";
 
 function App({}) {
-	let { data } = useQuery<NavigationData>(NAVIGATION_QUERY);
-	if (data) {
-		console.log(
-			"Navigation",
-			data.navigation.body.filter(l => l.type === "menu_link")
-		);
-	}
 	return (
 		<div className="app">
+			<Header />
 			<h1>App Updated!</h1>
 		</div>
 	);
 }
-
-const NAVIGATION_QUERY = gql`
-	query NavigationData {
-		navigation(uid: "navigation", lang: "en-us") {
-			title
-			logo
-			body {
-				... on NavigationBodyMenu_link {
-					type
-					primary {
-						label
-						path
-					}
-				}
-				... on NavigationBodySocial_media {
-					type
-					primary {
-						name
-						link
-					}
-				}
-			}
-		}
-	}
-`;
 
 export default App;
--- tasks/codegen.ps1
+++ tasks/codegen.ps1
@@ -1,2 +1,2 @@
 node tasks/generateSchema.js
-npx apollo codegen:generate --localSchemaFile=schema.json --target=typescript --globalTypesFile=src/__generated__/graphql-global-types.ts
+npx apollo codegen:generate --localSchemaFile=schema.json --target=typescript --globalTypesFile=src/_types/GlobalTypes.d.ts _types
